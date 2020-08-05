73fc86d791ad141dff58b541293d112873805fe3
panch0us <godbryansk@yandex.ru>
2020-08-05 21:21:08 +0300

    Изменение подсчетов совпадений по ГРЗ с использованием Counter from collections Python)

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pandas
+from collections import Counter
 
 list_grz_1 = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/targets_05.08.2020 12_05_21.csv',
                              encoding='utf_16_le')
 list_grz_2 = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/targets_05.08.2020 12_13_40.csv',
                              encoding='utf_16_le')
+print(type(list_grz_2))
 
-i = 0
-z = 0
-for grz_1 in list_grz_1['ГРЗ']:
-    for grz_2 in list_grz_2['ГРЗ']:
-        if grz_1 == grz_2:
-            print('ГРЗ_1: ' + grz_1 + ' --- ГРЗ_2: ' + grz_2)
+test_list_1 = []
+for el in list_grz_1['ГРЗ']:
+    test_list_1.append(el)
 
-    i = i + 1
+test_list_2 = []
+for el in list_grz_2['ГРЗ']:
+    test_list_2.append(el)
+
+result_counter_1 = Counter(test_list_1)
+result_counter_2 = Counter(test_list_2)
+
+print(result_counter_1['н408кр32'])
+
+
+for grz_1 in test_list_1:
+    if grz_1 in test_list_2:
+        print('ГРЗ_1: ' + grz_1 + ' -- количество этого ГРЗ во втором списке: ' + str(Counter(test_list_2)[grz_1]))
