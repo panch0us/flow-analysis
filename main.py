c31dda90ac14de92a613340c5626108e7b133a22
panch0us <godbryansk@yandex.ru>
2020-08-06 22:06:38 +0300

    Добавлена функция, которая принимает любое количество файлов и возвращает всё в нумерованный словарь

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas
 # имортируем встроенный класс Counter из модуля collection, который подсчитывает количество вхождений
 # элементов в списке. (Синтаксис: a = Counter(список); a.['нужный элемент списка для подсчета'])
 from collections import Counter
-
+"""
 list_grz_1 = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/targets_05.08.2020 12_05_21.csv',
                              encoding='utf_16_le')
 list_grz_2 = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/targets_05.08.2020 12_13_40.csv',
@@ -23,3 +23,37 @@ for grz_1 in set(test_list_1):
     if grz_1 in test_list_2:
         print(str(i) + ') ГРЗ_1: ' + grz_1 + ' -- количество этого ГРЗ во втором списке: ' + str(Counter(test_list_2)[grz_1]))
         i = i + 1
+"""
+
+def return_dict_all_grz():
+    """Функция для приема любого количества списков ГРЗ, переработки их в нумерованный словарь,
+    и возвращении полногго словаря с нумерованными списками ГРЗ"""
+    stop = ''
+    counter = 1
+    list_input = []
+    dict_input = {counter: list_input}
+    while stop == '':
+        list_input = []
+        file = input('Введити название файла: ')
+        #result = pandas.read_csv('C:/Users/umvd/Desktop/Анализ_потоков/csv/номер/' + file, encoding='utf_16_le')
+        result = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/' + file, encoding='utf_16_le')
+        for el in result['ГРЗ']:
+            list_input.append(el)
+            dict_input[counter] = list(list_input)
+        stop = input('Введите любой символ для завершения добавления файлов: ')
+        #print(dict_input[counter])
+        counter = counter + 1
+    return dict_input
+
+dict_input = return_dict_all_grz()
+
+count_key_from_dict = int(len(dict_input))
+print(count_key_from_dict)
+
+x = 0
+for key, value in dict_input.items():
+    count = Counter(value)
+    for el in count:
+        print(count[el])
+
+
