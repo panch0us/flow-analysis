31e14f214daccd28d340fc43d7297ac562734d4c
panch0us <godbryansk@yandex.ru>
2020-08-10 17:38:43 +0300

    Удалены ненужные части тестового кода

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 import pandas
 # имортируем встроенный класс Counter из модуля collection, который подсчитывает количество вхождений
 # элементов в списке. (Синтаксис: a = Counter(список); a.['нужный элемент списка для подсчета'])
-from collections import Counter
-
-"""
-list_grz_1 = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/targets_05.08.2020 12_05_21.csv',
-                             encoding='utf_16_le')
-list_grz_2 = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/targets_05.08.2020 12_13_40.csv',
-                             encoding='utf_16_le')
-
-test_list_1 = []
-for el in list_grz_1['ГРЗ']:
-    test_list_1.append(el)
-
-test_list_2 = []
-for el in list_grz_2['ГРЗ']:
-    test_list_2.append(el)
-
-
-i = 0
-# используем set (множество) для удаления повторяющихся элементов в списке
-for grz_1 in set(test_list_1):
-    if grz_1 in test_list_2:
-        print(str(i) + ') ГРЗ_1: ' + grz_1 + ' -- количество этого ГРЗ во втором списке: ' + str(Counter(test_list_2)[grz_1]))
-        i = i + 1
-"""
+#from collections import Counter
 
 
 def return_dict_all_grz():
