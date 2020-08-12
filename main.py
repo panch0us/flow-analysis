3c7938b3b45d07f86b3b15a77219b269baabc997
panch0us <godbryansk@yandex.ru>
2020-08-12 21:30:05 +0300

    Добавлена функция iteration_summ_list, которая принимает словарь вида {номер: [список ГРЗ]} и начиная с первого списка, проверяет его на совпадения со списками 2, 3, 4 и т.д., после чего список № 2 проверяется на совпадения со списком № 3, 4, 5 и т.д.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def return_dict_all_grz():
         file = input('Введити название файла: ')
         try:
             # result = pandas.read_csv('C:/Users/umvd/Desktop/Анализ_потоков/csv/номер/' + file, encoding='utf_16_le')
-            result = pandas.read_csv('C:/Users/asus/Desktop/home/test/potok/csv/номер/' + file, encoding='utf_16_le')
+            result = pandas.read_csv('C:/Users/asus/Desktop/home/test/analiz/csv/number/' + file, encoding='utf_16_le')
             # result = pandas.read_csv('C:/Users/panchous/Desktop/home/test/potok/csv/number/' + file, encoding='utf_16_le')
             for el in result['ГРЗ']:
                 list_input.append(el)
@@ -28,13 +28,15 @@ def return_dict_all_grz():
             continue
     return dict_input
 
+dict_input = return_dict_all_grz()
 
-def iteration_one_to_many_lists(func_return_dict_all_grz):
+
+def iteration_one_to_many_lists(dict_input_user):
     """Функция получает словарь, состоящий из {номера списка: списка ГРЗ} из функции return_dict_all_grz.
     После проводит итерацию по каждому полученному списку ГРЗ, сравнивая каждый номер первого списка со следующим
     по порядку списком."""
     # Получаем словарь вида {№: '[список ГРЗ]} из ввоада пользователя
-    dict_input = func_return_dict_all_grz()
+    dict_input = dict_input_user
     # Считаем количество списков в ГРЗ в полученом словаре
     count_element_from_dict = len(dict_input)
     i = 0
@@ -51,6 +53,38 @@ def iteration_one_to_many_lists(func_return_dict_all_grz):
                     print(str(el) + '\t из списка № ' + str(i) + '\t---> совпадение со списком № ' + str(b))
             b = b + 1
         b = 1 + i
+    return dict_input
 
 
-iteration_one_to_many_lists(return_dict_all_grz)
+def iteration_summ_list(dict_input_user):
+    a = 1
+    b = 2
+    i = 0
+    dict_input = dict_input_user
+    while a < len(dict_input):
+        print('------ СОВПАДЕНИЯ СПИСКА № ' + str(a) + ' -------')
+        while i < len(dict_input) and b < (len(dict_input) + 1):
+            if len(set(dict_input[a]) & set(dict_input[b])) > 0:
+                print('Совпадения между списком № ' + str(a) + ' и списком № ' + str(b))
+                print(set(dict_input[a]) & set(dict_input[b]))
+                result = list(set(dict_input[a]) & set(dict_input[b]))
+                b = b + 1
+                while i < len(dict_input) and b < (len(dict_input) + 1):
+                    if len(set(result) & set(dict_input[b])) > 0:
+                        print('Совпадения предыдущих списков со списокм № ' + str(b))
+                        print(set(result) & set(dict_input[b]))
+                        result = list(set(result) & set(dict_input[b]))
+                        b = b + 1
+                    else:
+                        print('Нет совпадений предыдущих списков со списокм № ' + str(b))
+                        b = b + 1
+                    i = i + 1
+            else:
+                print('Совпадения между списком № ' + str(a) + ' и списком № ' + str(b) + ' НЕТ')
+                b = b + 1
+        a = a + 1
+        b = a + 1
+
+
+first = iteration_one_to_many_lists(dict_input)
+iteration_summ_list(dict_input)
